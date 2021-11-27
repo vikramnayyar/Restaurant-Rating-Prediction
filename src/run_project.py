fb4b8e35ab7cb6b419eba4de30704fc8713ac558
Vikram Nayyar <vikramnayyar@live.com>
2021-11-27 09:27:13 +0530

    Update run_project.py

--- src/run_project.py
+++ src/run_project.py
@@ -9,9 +9,9 @@ import subprocess
 
 program_list = ['get_data.py', 'data_analysis.py', 'prepare_data.py', 'split_data.py', 'model_data.py']
 
-#for program in program_list:
-#    subprocess.call(['python3', program])
-#    print("Finished:" + program)
+for program in program_list:
+    subprocess.call(['python3', program])
+    print("Finished:" + program)
 
 app = '../app/app.py'
 subprocess.call(['streamlit', 'run',  app])
